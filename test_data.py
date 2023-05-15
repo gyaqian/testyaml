a9938669efaa96aef39cfedc3a21295c22d063c7
zhangsan <783032878@qq.com>
2023-05-16 00:14:43 +0800

    yaml文件读取，数据驱动

---
+++ test_data.py
@@ -0,0 +1,18 @@
+# _*_ coding:utf-8 _*_
+import yaml
+import pytest
+
+
+datas = yaml.safe_load(open('./data.yaml'))
+print(datas)
+
+class TestDemo():
+    @pytest.mark.parametrize('env', datas)
+    def test_demo(self,env):
+        if 'test' in env:
+            print("这是测试环境")
+            print(env["test"])
+
+        elif 'Dev' in env:
+            print("这是开发环境")
+            print(env["Dev"])
